4a09071ce69857d9ed0d1c399e9bd03ef4165b93
Lu <1625196521@qq.com>
2020-09-21 22:03:36 +0800

    4-class done

--- 4-class/practice_3/student.js
+++ 4-class/practice_3/student.js
@@ -1,1 +1,13 @@
 // TODO 13: 在这里写实现代码
+import Person from './person';
+
+export default class student extends Person {
+  constructor(name, age, sclass) {
+    super(name, age);
+    this.class = sclass;
+  }
+
+  introduce() {
+    return `I am a Student. I am at Class ${this.sclass}.`;
+  }
+}
--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,16 @@
 // TODO 20: 在这里写实现代码
+import Person from './person';
+
+export default class Worker extends Person {
+  constructor(name, age, sclass = 0) {
+    super(name, age);
+    this.sclass = sclass;
+  }
+
+  introduce() {
+    if (this.sclass) {
+      return `${super.introduce()} I am a Teacher. I teach Class ${this.sclass}.`;
+    }
+    return `${super.introduce()} I am a Teacher. I teach No Class.`;
+  }
+}
